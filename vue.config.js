ef585606c5b1a6141198d048ae9bdc32f45c9679
xiaoyoutiao <xiaoyoutiaoupup@gmail.com>
2021-11-22 23:04:12 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
     publicPath: isProd
-        ? 'https://xiaoyoutiao.github.io/'
+        ? '/study-notes/'
         : '/'
 }
