7ab91601869fa2eb07c3dba942fc16ac880c9201
huangqingyu <247316271@qq.com>
2021-11-22 22:36:41 +0800

    chore: inital

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>#Ryu#</h1>
+    <h1>Welcome to My Study Notes</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #2c3e50;
+}
+</style>
